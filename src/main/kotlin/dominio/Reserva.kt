918aca3f5db2c0d47150ffa7d55e5a1e82012079
aaron050223 <agallardoc@iesrafaelalberti.es>
2026-03-18 18:01:23 +0100

    Añado Kdoc

--- src/main/kotlin/dominio/Reserva.kt
+++ src/main/kotlin/dominio/Reserva.kt
@@ -3,6 +3,15 @@ package org.practicatrim2.dominio
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+/**
+ * Representa una reserva genérica.
+ *
+ * Esta clase es de tipo [abstract], por lo que no se puede instanciar directamente.
+ * Crea la estructura base de la clase para subclases de reservas como vuelos u hoteles.
+ *
+ * @property id Identificador único que se crea automáticamente.
+ * @property fechaCreacion Fecha en la que se registró la reserva en formato "dd-MM-yyyy".
+ */
 abstract class Reserva {
     companion object {
         private var contador = 0
